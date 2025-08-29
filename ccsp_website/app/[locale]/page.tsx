d1e80bb625a79202e9b8ba1553425fec593811d2
dana <lydana32@gmail.com>
2025-08-29 17:50:12 +0700

    feat: setup switch lang between kh and en

--- ccsp_website/app/[locale]/page.tsx
+++ ccsp_website/app/[locale]/page.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import Home from '../components/Home/Home'
+import Home from '@/components/Home/Home'
 
 const Homepage = () => {
   return <Home/>
+  
 }
 
 export default Homepage
